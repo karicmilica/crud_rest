68c02de2fe18aa972a6abea8d66e2ad5e1b6ffee
milica.karic <karic.milica@gmail.com>
2017-10-15 11:46:20 +0200

    wildfly config

--- src/main/java/rs/mk/CrudRestApplication.java
+++ src/main/java/rs/mk/CrudRestApplication.java
@@ -1,18 +1,29 @@
 package rs.mk;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 
 @SpringBootApplication
-public class CrudRestApplication {
+public class CrudRestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudRestApplication.class, args);
 	}
+	
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<CrudRestApplication> applicationClass = CrudRestApplication.class;
 	
 	@Bean
 	public Module hibernate4Module()
